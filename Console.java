8ad6280770578702953b4dc18874886d7eb430d5
Pueyo99 <55914406+Pueyo99@users.noreply.github.com>
2020-03-06 09:20:07 +0100

    Implementat el model MVC.

---
+++ Console.java
@@ -0,0 +1,21 @@
+import java.util.Observer;
+import java.util.Observable;
+
+public class Console implements Observer{
+
+    private LineM l;
+
+    public Console(LineM l){
+        this.l = l;
+    }
+
+    public void update(Observable obs, Object obj){
+        System.out.print("\u001b[1000D");
+        System.out.print("\u001b[0K");
+        System.out.print(l.getLine());
+        System.out.print("\u001b[1000D");
+        if(l.getIndex()!=0){
+            System.out.print("\u001b[" + l.getIndex() + "C");
+        }
+    }
+}
