b5c836fcf0162ec043bfde552c94a0e2a53a2436
Pueyo99 <55914406+Pueyo99@users.noreply.github.com>
2020-04-12 17:49:57 +0200

    Arxius principals del projecte.

---
+++ ProjecteJS/Client.js
@@ -0,0 +1,74 @@
+var W3CWebSocket = require('websocket').w3cwebsocket;
+
+function listenEnter(input,button){
+    var input = document.getElementById(input);
+    input.addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+          event.preventDefault();
+          document.getElementById(button).click();
+        }
+    });
+}
+
+function login(){
+    username = document.getElementById("nick").value;
+    if(username.length!=0){
+        localStorage.setItem("username",username);
+        window.location.replace("xat.html");
+    }else{
+        alert("No se ha introducido el Nick")
+    }
+}
+
+function start(){
+    var user = localStorage.getItem("username");
+    client = new W3CWebSocket('ws://localhost:8000/' + user);
+    conversation = new String();
+
+    document.getElementById("send").addEventListener("click",sendMessage);
+    document.getElementById("close").addEventListener("click",close);
+    document.getElementById("user").innerHTML = user;
+
+
+    function sendMessage(){
+        if (client.readyState === client.OPEN){
+            m = document.getElementById("message").value
+            if(m.length!=0){
+                document.getElementById("message").value = "";
+                client.send("-> "+ user + ": " + m);
+                conversation = conversation + "\n-> Yo: " + m;
+                document.getElementById("received").innerHTML = conversation;
+            }else{
+                alert("No hay mensajes a enviar");
+            }
+        }
+    }
+
+    function close(){
+        client.send("-> "+ user + " ha salido del xat");
+        client.close();
+    }
+
+    client.onopen = function(){
+        client.send("-> "+ user + " ha entrado en el xat");
+        conversation = "-> Has entrado en el xat";
+        document.getElementById("received").innerHTML = conversation;
+    }
+
+    client.onclose = function(){
+        window.location.replace("index.html");
+    };
+
+    client.onmessage = function(e){
+        if(typeof e.data === 'string'){
+            conversation = conversation + "\n" + e.data;
+            document.getElementById("received").innerHTML = conversation;
+        }
+    };
+
+    }
+
+    //Variables pel correcte funcionament de Browserify
+    global.login = login;
+    global.start = start;
+    global.listenEnter = listenEnter;
---
+++ ProjecteJS/Server.js
@@ -0,0 +1,45 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+const url = require('url');
+
+var users = new Map();
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(200);
+    response.end();
+});
+
+server.listen(8000, function() {
+    console.log((new Date()) + ' Server is listening on port 8000');
+});
+
+serversocket = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: false,
+});
+
+serversocket.on('request', function(request) {
+
+    connection = request.accept(null, request.origin);
+    username = url.parse(request.resourceURL).pathname.split("/")[1];
+    users.set(username,connection);
+    console.log((new Date()) + ' Connection accepted from: '+username);
+
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+
+            for(let con of users.values()){
+                if(con!=this){
+                    con.sendUTF(message.utf8Data);
+                }
+            }
+        }
+    });
+
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+
+    });
+});
